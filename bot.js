0da0f5090e45fd165aa2eede395a4e7bdbcd7a91
Lucas V <lucasjvicic@gmail.com>
2020-04-05 16:14:54 -0400

    updated response for bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,17 @@ function answerQuery(query) {
   // We are using a library called 'voca' (see https://vocajs.com) which has
   // many useful functions for working with strings. The following first converts
   // 'query' to lover-case and then tests if the query contains various keywords
-  // using 'v.includes'. You call all 'voca' functions by typing 'v.<some function>'/  
+  // using 'v.includes'. You call all 'voca' functions by typing 'v.<some function>'/
   query = v.lowerCase(query);
   if (v.includes(query, "rain") || v.includes(query, "sun") || v.includes(query, "weather"))
     return "I do not care too much about weather, I'm locked inside a data center.";
-  return "Sorry Dave, I cannot do that."
+  return "I would love some tea, but they have not created one for silicon-based life forms yet."
 }
 
 
+
 function isPermitted(query) {
-  // You will need to implement this function in the last part of the exercise 
+  // You will need to implement this function in the last part of the exercise
   // (after you add 'filtering.js' tests); you can ignore it until then!
   return true;
 }
@@ -24,7 +25,7 @@ function handleSayClick() {
   // which we are using for showing all the conversation history
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  
+
   // Check that the message does not contain any nasty words!
   if (!isPermitted(message)) {
     // If it does, just tell the user that we ignored their message
@@ -38,14 +39,14 @@ function handleSayClick() {
 }
 
 
-// If we are running in a browser, add onclick handler 
+// If we are running in a browser, add onclick handler
 // (if we are running tests, this will not do anything)
 if (typeof document != "undefined") {
   document.getElementById("sayit").onclick = handleSayClick;
 }
 
 // Export the two functions that we need to access from test files!
-module.exports = { 
-  answerQuery: answerQuery, 
+module.exports = {
+  answerQuery: answerQuery,
   isPermitted: isPermitted
 }
